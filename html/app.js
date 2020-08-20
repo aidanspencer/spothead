43633ab4e6e5dc9fa2e0c5c9192eb1d27c5fd3d4
aidanspencer <aidanspencer@gmail.com>
2020-08-20 12:05:00 -0400

    Adding Link to beta

---
+++ html/app.js
@@ -0,0 +1,56 @@
+"use strict";
+
+let map, infoWindow, kmlLayer;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 40.73,
+      lng: -73.98
+    },
+    zoom: 14
+  });
+
+    var src = 'https://raw.githubusercontent.com/aidanspencer/spothead/master/Spothead%20Folderless.kml';
+    kmlLayer = new google.maps.KmlLayer(src, {
+        suppressInfoWindows: true,
+        preserveViewport: false,
+        map: map
+    });
+    kmlLayer.addListener('click', function(event) {
+        var content = event.featureData.infoWindowHtml;
+        var testimonial = document.getElementById('info');
+        testimonial.innerHTML = content;
+    });
+
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Your Location');
+      infoWindow.open(map);
+      map.setCenter(pos);
+      map.zoom = 14;
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
